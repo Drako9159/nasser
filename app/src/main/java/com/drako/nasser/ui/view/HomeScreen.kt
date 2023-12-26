c72809dd22dc80731facf924db92a94ca4993f5f
drako9159 <chochito9159@gmail.com>
2023-12-26 17:19:25 -0600

    feat(nasser) configure iterface for nasser home

--- app/src/main/java/com/drako/nasser/ui/view/HomeScreen.kt
+++ app/src/main/java/com/drako/nasser/ui/view/HomeScreen.kt
@@ -1,9 +1,14 @@
 package com.drako.nasser.ui.view
 
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,22 +18,39 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.drako.nasser.R
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+
 fun HomeScreen() {
     SetStatusBarColor(color = colorResource(id = R.color.amoled_black))
     Scaffold(
-        topBar = { Toolbar() },
-        content = { Content() }
-    )
+        topBar = { Toolbar() }
+    ) {
+        innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+
+                    It also contains some basic inner content, such as this text.
+
+                    You have pressed the floating action button 12 times.
+                """.trimIndent(),
+            )
+        }
+    }
 }
 
 
@@ -36,7 +58,7 @@ fun HomeScreen() {
 @Composable
 fun Toolbar() {
     TopAppBar(
-        title = { Text(text = "NASSER") },
+        title = { Text(text = stringResource(id = R.string.app_name_toolbar)) },
         colors = TopAppBarDefaults.largeTopAppBarColors(
             containerColor = colorResource(id = R.color.amoled_black),
             titleContentColor = colorResource(id = R.color.white)
@@ -47,15 +69,44 @@ fun Toolbar() {
 
 @Composable
 fun Content() {
+    /*
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Box(
+            modifier = Modifier.fillMaxWidth().background(color = Color.Blue),
+            contentAlignment = Alignment.Center
+        ) {
+            //Text(text = "HelloWorld")
+            Box(
+                modifier = Modifier.fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(20.dp))
+                    .background(color = Color.Red)
+                    .border(BorderStroke(2.dp, SolidColor(Color.Red)))
+                    .padding(16.dp)
+            ) {
+                Text(text = "Server status is off")
+            }
 
-
-    Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
-
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = "HelloWorld")
         }
 
+    }*/
+
+    Column(modifier = Modifier.padding(8.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text =
+            """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+
+                    It also contains some basic inner content, such as this text.
+
+                    You have pressed the floating action button 12 times.
+                """.trimIndent(),
+        )
     }
+
+
+
+
 }
 
 @Composable
@@ -65,3 +116,4 @@ fun SetStatusBarColor(color: Color) {
         systemUiController.setSystemBarsColor(color)
     }
 }
+
